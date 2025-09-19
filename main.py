9d696c1f676354f96ab7069e0724476074bea191
Gabriel da Silva <gabriellsilva1406@gmail.com>
2025-09-19 16:01:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+while True:
+    res = ''
+    # Menu.
+    print('-' * 50)
+    print('TABUADA'.center(50))
+    print('-' * 50)
+    try:
+        # Irá tentar ler um número inteiro, caso não seja ou o número seja menor que 0, será exibido uma mensagem de erro.
+        num = int(input('Digite um número: '))
+    except:
+        print('ERRO! Valor digitado inválido')
+    else:
+        if num < 0:
+            print('ERRO! Valor digitado inválido')
+        else:
+            # Exibição da tabuada.
+            print('-' * 50)
+            for c in range(1, 11):
+                print(f'{num} x {c} = {num * c}')
+        print('-' * 50)
+        # Perguntar ao usúario se ele quer continuar, caso sim, o programa continua, caso não, o programa termina.
+        while res not in ('S', 'N'):
+            res = str(input('Quer continuar [S/N]? ').strip().upper())
+            # Caso não seja S ou N será exibido uma mensagem de erro.
+            if res not in ('S', 'N'):
+                print('ERRO! Valor digitado inválido')
+                print('-' * 50)
+        if res in 'N':
+            break
+# Final do programa.
+print('-' * 50)
+print('OBRIGADO POR USAR MINHA TABUADA.'.center(50))
+print('-' * 50)
